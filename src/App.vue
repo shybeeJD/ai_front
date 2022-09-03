f2e5b9ec3f234d83a5829b1abff04c2d9aa7796a
shybeeJD <shybeejd@gmail.com>
2022-09-03 17:11:24 +0800

    nav Meun

--- src/App.vue
+++ src/App.vue
@@ -1,169 +1,219 @@
 <template>
-
-  <div>
-      <!-- 头部区域 -->
-      <div class="header w">
-          <!-- logo部分 -->
-          <div class="logo">
-              <img src="../static/images/logo.jpg">
-          </div>
-          <!-- 导航栏部分 nav -->
-          <div class="nav">
-              <ul>
-                  <li><a href="#1">图像攻防</a></li>
-                  <li><a href="#2">语音攻防</a></li>
-                  <li><a href="#3">可解释性</a></li>
-              </ul>
-          </div>
-      </div>
-      <!-- 头部区域 -->
-      <hr>
-      <!-- 下边页面 -->
-      <div class="w">
-          <div class="page " id="1" style="display: block;">
-              <h1>图像攻防</h1>
-              <div class="box">
-                  <ul class="clearfix">
-                      <li class="box_transition">
-                          过渡
-                      </li>
-                      <li class="box_body">
-                          攻击
-                      </li>
-                      <li class="box_transition">
-                          过渡
-                      </li>
-                      <li class="box_body">
-                          检测
-                      </li>
-                      <li class="box_transition">
-                          过渡
-                      </li>
-                      <li class="box_body">
-                          擦除
-                      </li>
-                      <li class="box_transition">
-                          过渡
-                      </li>
-                      <li class="box_transition">
-                          过渡
-                      </li>
-                      <li class="box_body">
-                          模型增强
-                      </li>
-                      <li class="box_transition">
-                          过渡
-                      </li>
-                      <li class="box_body">
-                          后门
-                      </li>
-                      <li class="box_transition">
-                          过渡
-                      </li>
-                      <li class="box_body">
-                          投毒
-                      </li>
-                      <li class="box_transition">
-                          过渡
-                      </li>
-                  </ul>
-              </div>
-          </div>
-          <div class="page" id="2" style="display: none;">
-              <h1>语音攻防</h1>
-          </div>
-          <div class="page" id="3" style="display: none;">
-              <h1>可解释性</h1>
-          </div>
-      </div>
-  </div>
-  </template>
+    <div>
+        <el-row type="flex" justify="start">
+            <el-col :span="4">
+                <div class="logo">
+                    <img src="../static/images/logo.jpg">
+                </div>
+            </el-col>
+            <el-col :span="12">
+    <el-menu
+    background-color="#545c64"
+        mode="horizontal"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+        :default-active="activeIndex"
+        router>
+    <NavMenu :navMenus="menuData"></NavMenu>
+      </el-menu>
+      <router-view></router-view>
+            </el-col>
+        </el-row>
+    </div>
+</template>
 <script>
+import NavMenu from './components/NavMenu.vue'
 export default {
-  name: 'AIIndex',
+  components: {
+    NavMenu: NavMenu
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      activeIndex: 'aa',
+      menuData: [
+        {
+          // 一级
+          entity: {
+            id: 0,
+            name: 'systemManage',
+            icon: 'el-icon-picture',
+            alias: '图像'
+          },
+          // 二级
+          childs: [
+            {
+              entity: {
+                id: 1,
+                name: 'pic1',
+                icon: 'el-icon-loading',
+                alias: '攻击',
+                value: { path: '/hello' }
+              }
+            },
+            {
+              entity: {
+                id: 2,
+                name: 'pic2',
+                icon: 'el-icon-bell',
+                alias: '防御',
+                value: '/system/role'
+              }
+            },
+            {
+              entity: {
+                id: 3,
+                name: 'pic3',
+                icon: 'el-icon-edit',
+                alias: '擦除',
+                value: '/system/menu'
+              }
+            },
+            {
+              entity: {
+                id: 4,
+                name: 'pic4',
+                icon: 'el-icon-mobile-phone\r\n',
+                alias: '。。。',
+                value: '/system/group'
+              }
+            }
+          ]
+        },
+        {
+          // 一级
+          entity: {
+            id: 5,
+            name: 'audio',
+            icon: 'el-icon-bell',
+            alias: '语音'
+          },
+          // 二级
+          childs: [
+            {
+              entity: {
+                id: 6,
+                name: 'aud1',
+                icon: 'el-icon-loading',
+                alias: '攻击',
+                value: { path: '/hello' }
+              }
+            },
+            {
+              entity: {
+                id: 7,
+                name: 'aud2',
+                icon: 'el-icon-bell',
+                alias: '防御',
+                value: '/system/role'
+              }
+            },
+            {
+              entity: {
+                id: 8,
+                name: 'aud3',
+                icon: 'el-icon-edit',
+                alias: '擦除',
+                value: '/system/menu'
+              }
+            },
+            {
+              entity: {
+                id: 9,
+                name: 'aud4',
+                icon: 'el-icon-mobile-phone\r\n',
+                alias: '。。。',
+                value: '/system/group'
+              }
+            }
+          ]
+        },
+        {
+          // 一级
+          entity: {
+            id: 10,
+            name: 'explain',
+            icon: 'el-icon-view',
+            alias: '可解释性'
+          },
+          // 二级
+          childs: [
+            {
+              entity: {
+                id: 11,
+                name: 'ex1',
+                icon: 'el-icon-loading',
+                alias: '攻击',
+                value: { path: '/hello' }
+              }
+            },
+            {
+              entity: {
+                id: 12,
+                name: 'ex2',
+                icon: 'el-icon-bell',
+                alias: '防御',
+                value: '/system/role'
+              }
+            },
+            {
+              entity: {
+                id: 13,
+                name: 'ex3',
+                icon: 'el-icon-edit',
+                alias: '擦除',
+                value: '/system/menu'
+              }
+            },
+            {
+              entity: {
+                id: 14,
+                name: 'ex4',
+                icon: 'el-icon-mobile-phone\r\n',
+                alias: '。。。',
+                value: '/system/group'
+              }
+            }
+          ]
+        }
+      ]
     }
   }
 }
 </script>
-  <style>
-      * {
-      margin: 0;
-      padding: 0;
-  }
-  .w {
-      width: 1200px;
-      margin: auto;
-  }
-  li {
-      list-style: none;
-  }
-  a {
-      text-decoration: none;
-  }
-  .clearfix:before,.clearfix:after {
-      content:"";
-      display:table;
-    }
-    .clearfix:after {
-      clear:both;
-    }
-    .clearfix {
-       *zoom:1;
-    }
-  .header {
-      height: 42px;
-      margin: 30px auto;
-  }
-  .logo img{
+<style>
+.logo img{
+      position:absolute;
       float: left;
       width: 200px;
       height: auto;
+      z-index:100;
   }
-  .nav {
-      float: left;
-      margin-left: 230px;
-  }
-  .nav ul li {
-      float: left;
-      margin: 0 15px;
-  }
-  .nav ul li a {
-      display: block;
-      height: 40px;
-      padding: 0 10px;
-      line-height: 40px;
-      font-size: 20px;
-      color: #050505;
-  }
-  .nav ul li a:hover {
-      border-bottom: 2px solid #00a4ff;
-      color: #00a4ff;
-  }
-  .page h1{
-      padding-top: 20px;
-      text-align: center;
-  }
-  .page .box ul {
-      width: 1200px;
-      margin-top: 50px;
-      margin-left: 40px;
-  }
-  .page .box ul li {
-      float: left;
-      width: 150px;
-      height: 200px;
-      margin-right: 5px;
-      margin-left: 5px;
-      margin-bottom: 50px;
-      text-align: center;
-  }
-  .page .box_transition {
-      background: skyblue;
-  }
-  .page .box_body{
-      background-color: red;
-  }
-  </style>
+    /* 水平样式 */
+ .el-menu--horizontal>div>.el-submenu {
+    float: left;
+}
+/* 一级菜单的样式 */
+.el-menu--horizontal>div>.el-menu-item {
+    float: left;
+    height: 60px;
+    line-height: 60px;
+    margin: 0;
+    border-bottom: 2px solid transparent;
+    color: #909399;
+}
+/* 解决下图1 下拉三角图标 */
+.el-menu--horizontal>div>.el-submenu .el-submenu__icon-arrow {
+    position: static;
+    vertical-align: middle;
+    margin-left: 8px;
+    margin-top: -3px;
+}
+/* 解决下图2 无下拉菜单时 不对齐问题 */
+.el-menu--horizontal>div>.el-submenu .el-submenu__title {
+    height: 60px;
+    line-height: 60px;
+    border-bottom: 2px solid transparent;
+    color: #909399;
+}
+
+</style>
